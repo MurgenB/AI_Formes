4948e246ebe52fe1bb26fb3e44749712faf4bbd9
MurgenB <92163479+MurgenB@users.noreply.github.com>
2022-10-07 14:59:05 +0200

    GrapheAffiché

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Graphe Un = Graphe("D:\\IA\\Formes\\un.txt");
+foreach (char[] line in Un.matrix)
+{
+    foreach (char c in line)
+    {
+        Console.Write(c);
+    }
+    Console.WriteLine();
+}
